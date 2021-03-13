967fcbf0746fdb76855050585cfc76b624024fe8
Fr0z3Nn <luckyjohn057@gmail.com>
2021-03-13 12:23:34 +0300

    6 версия

--- src/main/java/ru/project/unpacker/service/UnpackService.java
+++ src/main/java/ru/project/unpacker/service/UnpackService.java
@@ -11,29 +11,13 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class UnpackService {
-    public static void main(String[] args) {
-        UnpackService unpackService = new UnpackService();
-        unpackService.unpack("1[c]5[v]");
-    }
-
-    volatile String stroke1;
-
     public String unpack(String stroke) {
         if (stroke == null || stroke.trim().isEmpty()) {
             throw new InvalidInputException("Invalid stroke");
         }
-        stroke1 = stroke;
-        Pattern.compile("(\\d+)\\[([^\\[\\]]+)\\]").matcher(stroke)
-                .results()
-                .map(matchResult -> {
-                    return new UnpackPair(Integer.parseInt(matchResult.group(1)), matchResult.group(2));
-                })
-                .collect(Collectors.toSet());
-
-
-        String regexp = "(\\d+\\[[^\\[\\]]+\\])";
+        String regexp = "(\\d+)\\[([^\\[\\]]+)\\]";
         if (Pattern.compile(regexp).matcher(stroke).find()) {
-            for (UnpackPair pair : Pattern.compile("(\\d+)\\[([^\\[\\]]+)\\]").matcher(stroke)
+            for (UnpackPair pair : Pattern.compile(regexp).matcher(stroke)
                     .results()
                     .map(matchResult -> new UnpackPair(Integer.parseInt(matchResult.group(1)), matchResult.group(2)))
                     .collect(Collectors.toSet())) {
